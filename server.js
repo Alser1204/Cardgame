d90310bde03dee2969ec9d11021506b6ced06b4f
Alser1204 <moomin12040829@icloud.com>
2025-09-05 22:51:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,19 +14,18 @@ const rooms = {};
 //   roomId: {
 //     players: [socket.id, ...],
 //     turnIndex: 0,
-//     hands: { socketId: ["カードA", "カードB", ...] }
+//     hands: { socketId: ["カードA", ...] },
+//     hp: { socketId: 10 }
 //   }
 // }
 
 io.on("connection", (socket) => {
   console.log("a user connected:", socket.id);
 
-  // ルーム参加
   socket.on("joinRoom", (roomId) => {
     if (!rooms[roomId]) {
-      rooms[roomId] = { players: [], turnIndex: 0, hands: {} };
+      rooms[roomId] = { players: [], turnIndex: 0, hands: {}, hp: {} };
     }
-
     const room = rooms[roomId];
     if (room.players.length >= 2) {
       socket.emit("roomFull");
@@ -34,9 +33,8 @@ io.on("connection", (socket) => {
     }
 
     room.players.push(socket.id);
-
-    // 簡単にカードを2枚ずつ配る
     room.hands[socket.id] = ["カード1", "カード2"];
+    room.hp[socket.id] = 10;
 
     socket.join(roomId);
     io.to(roomId).emit("message", `プレイヤーが参加しました (${room.players.length}/2)`);
@@ -45,7 +43,8 @@ io.on("connection", (socket) => {
     if (room.players.length === 2) {
       io.to(roomId).emit("message", "ゲーム開始！");
       const firstPlayer = room.players[room.turnIndex];
-      io.to(firstPlayer).emit("yourTurn", room.hands[firstPlayer]);
+      io.to(firstPlayer).emit("yourTurn", room.hands[firstPlayer], room.hp);
+      io.to(room.players[(room.turnIndex + 1) % 2]).emit("updateHP", room.hp);
     }
   });
 
@@ -67,29 +66,43 @@ io.on("connection", (socket) => {
       return;
     }
 
-    // カードを手札から除外
+    // 手札からカード削除
     room.hands[socket.id] = hand.filter(c => c !== card);
 
-    // 相手に通知
-    socket.to(roomId).emit("opponentPlayed", card);
+    // 相手ID
+    const opponentId = room.players.find(id => id !== socket.id);
+
+    // カード効果: 1ダメージ
+    room.hp[opponentId] -= 1;
+
+    // HP更新通知
+    io.to(roomId).emit("updateHP", room.hp);
+    io.to(roomId).emit("message", `${socket.id} が ${card} をプレイ！`);
+
+    // 勝利判定
+    if (room.hp[opponentId] <= 0) {
+      io.to(roomId).emit("message", `${socket.id} の勝利！`);
+      io.to(roomId).emit("gameOver", socket.id);
+      delete rooms[roomId];
+      return;
+    }
 
     // ターン交代
     room.turnIndex = (room.turnIndex + 1) % room.players.length;
     const nextPlayer = room.players[room.turnIndex];
-    io.to(nextPlayer).emit("yourTurn", room.hands[nextPlayer]);
+    io.to(nextPlayer).emit("yourTurn", room.hands[nextPlayer], room.hp);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected:", socket.id);
-    // 参加していたルームから削除
     for (const roomId in rooms) {
       const room = rooms[roomId];
       const index = room.players.indexOf(socket.id);
       if (index !== -1) {
         room.players.splice(index, 1);
         delete room.hands[socket.id];
+        delete room.hp[socket.id];
         io.to(roomId).emit("message", "プレイヤーが退出しました");
-        // ルームに誰もいなければ削除
         if (room.players.length === 0) delete rooms[roomId];
       }
     }
