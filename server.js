c4d93efc08e417568aa15096bded8a66d253ea9a
Alser1204 <moomin12040829@icloud.com>
2025-10-04 23:54:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -368,5 +368,5 @@ io.on("connection", (socket) => {
 // ==============================
 // サーバー起動
 // ==============================
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`listening on *:${PORT}`));
